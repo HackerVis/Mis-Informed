24f2047611777c6d4a9cf3c696576f7fadc82b36
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2023-02-05 21:13:26 -0700

    basic stuff

--- Mis-informed/main.py
+++ Mis-informed/main.py
@@ -1,24 +1,26 @@
-from flask import Flask, render_template,request,jsonify
-from flask_cors import CORS
+from flask import Flask, render_template,request,jsonify # Importing the Flask module
+from flask_cors import CORS # Importing the CORS module
+from Mis_informed import * # Importing the Mis_informed.py file
 
-app = Flask(__name__)
-CORS(app)
+app = Flask(__name__) # Creating a Flask object
+CORS(app) # Enabling CORS
 
 
-@app.route('/',methods = ['GET'])
+@app.route('/',methods = ['GET']) # The route for the home page
 def home():
-    return render_template('index.html')
+    return render_template('index.html') # The home page
 
-@app.route('/status', methods=['GET'])
+@app.route('/status', methods=['GET']) # The route for the status page
 def status():
 	return(jsonify(200))
 
-@app.route('/check/', methods=['GET', 'POST'])
-def check():
-    if request.method == 'POST':
+@app.route('/check/', methods=['GET', 'POST']) # The route for the check page
+def check(): 
+    if request.method == 'POST': 
         url = request.form['url']
-        print(url)
-        return render_template('index.html')
+        site = url
+        informative_percent, safetyResult, domain_name = getMisinformation(site)
+        return render_template('index.html', site = domain_name, informative_percent = informative_percent, safetyResult = safetyResult) # The check page
     else:
         return render_template('index.html')
 
